06b30a1b61a88f354089dcdc214f2cb6f0faf828
hitoricamiter <koshurnikovaslyam@mail.ru>
2025-06-01 01:29:40 +0600

    test tested

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -1,7 +1,10 @@
 package com.example.workflow;
 
+import org.camunda.bpm.application.ProcessApplicationDeploymentInfo;
+import org.camunda.bpm.engine.authorization.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,7 @@ public class Application {
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
   }
+
+
 
 }
